eb707cdc9038b14765b46e4b0d02078215310276
Jan Mas Rovira <janmasrovira@gmail.com>
2025-12-23 23:25:35 +0100

    add Semitopology definition

--- mlda.lean
+++ mlda.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `mlda` library.
 -- Import modules here that should be built as part of the library.
 import mlda.Three
-import mlda.Participants
+import mlda.Semitopology
