e8a324c65ce5cfb8c3ee5166a86271e4b87cf68a
Jan Mas Rovira <janmasrovira@gmail.com>
2026-03-12 16:15:06 +0100

    update pdf

--- mlda.lean
+++ mlda.lean
@@ -7,8 +7,8 @@ import mlda.Section5
 /-!
 # Modal Logic for Declarative Distributed Algorithms
 
-A full Lean 4 formalisation of the paper *"Declarative distributed broadcast
-using three-valued modal logic and semitopologies"* by Murdoch J. Gabbay.
+A full Lean 4 formalisation of the paper *"Declarative distributed algorithms
+as axiomatic theories in three-valued modal logic over semitopologies"* by Murdoch J. Gabbay.
 
 ## Overview
 
